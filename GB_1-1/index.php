153b74f7dcb905cc18e65de8966c3831f8ec4c47
MIXALK <mixalka01@gmail.com>
2017-01-14 03:15:06 +0300

    Добавили задание 1-1

---
+++ GB_1-1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <!--С помощью оператора echo выведите на страницу:
+    a. Целочисленную переменную-->
+    <p><?php
+    $a=1;
+    echo $a;
+    ?></p>
+
+</body>
+</html>
